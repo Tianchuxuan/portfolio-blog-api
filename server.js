a557b66d1cdf36e71e8f90e9f31cedf68762f62e
Tianchuxuan <yesterday6333@qq.com>
2025-11-18 15:51:48 +0800

    修复 CORS 跨域配置

--- server.js
+++ server.js
@@ -18,7 +18,13 @@ const app = express();
 
 
 app.use(helmet()); 
-app.use(cors()); 
+app.use(
+  cors({
+    origin: 'https://react-al4d68sqw-tianchuxuans-projects.vercel.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+  })
+);
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true })); 
 
